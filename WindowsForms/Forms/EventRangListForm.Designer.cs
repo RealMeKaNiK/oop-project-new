ef794aa13efc5fac28595155a94a4ecc29587ad6
RealMeKaNiK <dautovic2000@gmail.com>
2022-04-14 09:36:00 +0200

    changing button layout

--- WindowsForms/Forms/EventRangListForm.Designer.cs
+++ WindowsForms/Forms/EventRangListForm.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvEventStats = new System.Windows.Forms.DataGridView();
-            this.btnPrint = new System.Windows.Forms.Button();
-            this.matchBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.locationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.attendanceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.hometeamcountryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.awayteamcountryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.matchBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnPrint = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEventStats)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.matchBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -49,9 +49,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(328, 98);
+            this.label1.Location = new System.Drawing.Point(492, 151);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(329, 29);
+            this.label1.Size = new System.Drawing.Size(485, 40);
             this.label1.TabIndex = 1;
             this.label1.Text = "Rang Lista / Event Statisika";
             // 
@@ -67,79 +68,87 @@
             this.hometeamcountryDataGridViewTextBoxColumn,
             this.awayteamcountryDataGridViewTextBoxColumn});
             this.dgvEventStats.DataSource = this.matchBindingSource;
-            this.dgvEventStats.Location = new System.Drawing.Point(12, 171);
+            this.dgvEventStats.Location = new System.Drawing.Point(18, 263);
+            this.dgvEventStats.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvEventStats.Name = "dgvEventStats";
             this.dgvEventStats.ReadOnly = true;
-            this.dgvEventStats.Size = new System.Drawing.Size(960, 446);
+            this.dgvEventStats.RowHeadersWidth = 62;
+            this.dgvEventStats.Size = new System.Drawing.Size(1440, 686);
             this.dgvEventStats.TabIndex = 2;
-            // 
-            // btnPrint
-            // 
-            this.btnPrint.Location = new System.Drawing.Point(819, 692);
-            this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(153, 57);
-            this.btnPrint.TabIndex = 3;
-            this.btnPrint.Text = "Print";
-            this.btnPrint.UseVisualStyleBackColor = true;
-            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
-            // 
-            // matchBindingSource
-            // 
-            this.matchBindingSource.DataSource = typeof(DataAccessLayer.Model.Match);
             // 
             // locationDataGridViewTextBoxColumn
             // 
             this.locationDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.locationDataGridViewTextBoxColumn.DataPropertyName = "location";
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.locationDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle5;
             this.locationDataGridViewTextBoxColumn.HeaderText = "Location";
+            this.locationDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.locationDataGridViewTextBoxColumn.Name = "locationDataGridViewTextBoxColumn";
             this.locationDataGridViewTextBoxColumn.ReadOnly = true;
-            this.locationDataGridViewTextBoxColumn.Width = 73;
+            this.locationDataGridViewTextBoxColumn.Width = 106;
             // 
             // attendanceDataGridViewTextBoxColumn
             // 
             this.attendanceDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.attendanceDataGridViewTextBoxColumn.DataPropertyName = "attendance";
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.attendanceDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.attendanceDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle6;
             this.attendanceDataGridViewTextBoxColumn.HeaderText = "Attendance";
+            this.attendanceDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.attendanceDataGridViewTextBoxColumn.Name = "attendanceDataGridViewTextBoxColumn";
             this.attendanceDataGridViewTextBoxColumn.ReadOnly = true;
-            this.attendanceDataGridViewTextBoxColumn.Width = 87;
+            this.attendanceDataGridViewTextBoxColumn.Width = 128;
             // 
             // hometeamcountryDataGridViewTextBoxColumn
             // 
             this.hometeamcountryDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.hometeamcountryDataGridViewTextBoxColumn.DataPropertyName = "home_team_country";
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hometeamcountryDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hometeamcountryDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle7;
             this.hometeamcountryDataGridViewTextBoxColumn.HeaderText = "Home Team Country";
+            this.hometeamcountryDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.hometeamcountryDataGridViewTextBoxColumn.Name = "hometeamcountryDataGridViewTextBoxColumn";
             this.hometeamcountryDataGridViewTextBoxColumn.ReadOnly = true;
-            this.hometeamcountryDataGridViewTextBoxColumn.Width = 118;
+            this.hometeamcountryDataGridViewTextBoxColumn.Width = 175;
             // 
             // awayteamcountryDataGridViewTextBoxColumn
             // 
             this.awayteamcountryDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.awayteamcountryDataGridViewTextBoxColumn.DataPropertyName = "away_team_country";
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.awayteamcountryDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.awayteamcountryDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle8;
             this.awayteamcountryDataGridViewTextBoxColumn.HeaderText = "Away Team Country";
+            this.awayteamcountryDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.awayteamcountryDataGridViewTextBoxColumn.Name = "awayteamcountryDataGridViewTextBoxColumn";
             this.awayteamcountryDataGridViewTextBoxColumn.ReadOnly = true;
-            this.awayteamcountryDataGridViewTextBoxColumn.Width = 116;
+            this.awayteamcountryDataGridViewTextBoxColumn.Width = 171;
+            // 
+            // matchBindingSource
+            // 
+            this.matchBindingSource.DataSource = typeof(DataAccessLayer.Model.Match);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(1228, 134);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(230, 88);
+            this.btnPrint.TabIndex = 3;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
             // EventRangListForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.ClientSize = new System.Drawing.Size(1476, 1106);
             this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.dgvEventStats);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "EventRangListForm";
             this.Text = "EventRangListForm";
             this.Load += new System.EventHandler(this.EventRangListForm_Load);
--- WindowsForms/Forms/PlayersRangListForm.Designer.cs
+++ WindowsForms/Forms/PlayersRangListForm.Designer.cs
@@ -31,13 +31,13 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvPlayerStats = new System.Windows.Forms.DataGridView();
-            this.btnPrint = new System.Windows.Forms.Button();
-            this.playerBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.shirtnumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.positionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.goalNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.yellowCardNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.playerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnPrint = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPlayerStats)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.playerBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -46,9 +46,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(332, 74);
+            this.label1.Location = new System.Drawing.Point(498, 114);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(343, 29);
+            this.label1.Size = new System.Drawing.Size(503, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "Rang Lista / Statistika igraca";
             // 
@@ -65,70 +66,84 @@
             this.goalNumberDataGridViewTextBoxColumn,
             this.yellowCardNumberDataGridViewTextBoxColumn});
             this.dgvPlayerStats.DataSource = this.playerBindingSource;
-            this.dgvPlayerStats.Location = new System.Drawing.Point(12, 153);
+            this.dgvPlayerStats.Location = new System.Drawing.Point(18, 235);
+            this.dgvPlayerStats.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvPlayerStats.Name = "dgvPlayerStats";
             this.dgvPlayerStats.ReadOnly = true;
-            this.dgvPlayerStats.Size = new System.Drawing.Size(960, 446);
+            this.dgvPlayerStats.RowHeadersWidth = 62;
+            this.dgvPlayerStats.Size = new System.Drawing.Size(1440, 686);
             this.dgvPlayerStats.TabIndex = 1;
-            // 
-            // btnPrint
-            // 
-            this.btnPrint.Location = new System.Drawing.Point(829, 700);
-            this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(143, 49);
-            this.btnPrint.TabIndex = 2;
-            this.btnPrint.Text = "Print";
-            this.btnPrint.UseVisualStyleBackColor = true;
-            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
-            // 
-            // playerBindingSource
-            // 
-            this.playerBindingSource.DataSource = typeof(DataAccessLayer.Model.Player);
             // 
             // nameDataGridViewTextBoxColumn
             // 
             this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
             this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
             this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 150;
             // 
             // shirtnumberDataGridViewTextBoxColumn
             // 
             this.shirtnumberDataGridViewTextBoxColumn.DataPropertyName = "shirt_number";
             this.shirtnumberDataGridViewTextBoxColumn.HeaderText = "Shirt number";
+            this.shirtnumberDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.shirtnumberDataGridViewTextBoxColumn.Name = "shirtnumberDataGridViewTextBoxColumn";
             this.shirtnumberDataGridViewTextBoxColumn.ReadOnly = true;
+            this.shirtnumberDataGridViewTextBoxColumn.Width = 150;
             // 
             // positionDataGridViewTextBoxColumn
             // 
             this.positionDataGridViewTextBoxColumn.DataPropertyName = "position";
             this.positionDataGridViewTextBoxColumn.HeaderText = "Position";
+            this.positionDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.positionDataGridViewTextBoxColumn.Name = "positionDataGridViewTextBoxColumn";
             this.positionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.positionDataGridViewTextBoxColumn.Width = 150;
             // 
             // goalNumberDataGridViewTextBoxColumn
             // 
             this.goalNumberDataGridViewTextBoxColumn.DataPropertyName = "GoalNumber";
             this.goalNumberDataGridViewTextBoxColumn.HeaderText = "Goal number";
+            this.goalNumberDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.goalNumberDataGridViewTextBoxColumn.Name = "goalNumberDataGridViewTextBoxColumn";
             this.goalNumberDataGridViewTextBoxColumn.ReadOnly = true;
+            this.goalNumberDataGridViewTextBoxColumn.Width = 150;
             // 
             // yellowCardNumberDataGridViewTextBoxColumn
             // 
             this.yellowCardNumberDataGridViewTextBoxColumn.DataPropertyName = "YellowCardNumber";
             this.yellowCardNumberDataGridViewTextBoxColumn.HeaderText = "Yellow card number";
+            this.yellowCardNumberDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.yellowCardNumberDataGridViewTextBoxColumn.Name = "yellowCardNumberDataGridViewTextBoxColumn";
             this.yellowCardNumberDataGridViewTextBoxColumn.ReadOnly = true;
+            this.yellowCardNumberDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // playerBindingSource
+            // 
+            this.playerBindingSource.DataSource = typeof(DataAccessLayer.Model.Player);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(1244, 114);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(214, 75);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
             // PlayersRangListForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.ClientSize = new System.Drawing.Size(1476, 1106);
             this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.dgvPlayerStats);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "PlayersRangListForm";
             this.Text = "PlayersRangListForm";
             this.Load += new System.EventHandler(this.PlayersRangListForm_Load);
